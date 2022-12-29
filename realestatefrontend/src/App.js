52028ab94f123e290075792b5461da2fa6564a5a
shindeamul76 <115431191+shindeamul76@users.noreply.github.com>
2022-12-29 22:13:40 +0530

    Update App.js
    updated some browser and router things

--- realestatefrontend/src/App.js
+++ realestatefrontend/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Signup from "./signUp/Signup";
 import Signin from './signUp/Signin';
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/Signup" element={<Signup/>} />
-        <Route path="/home" element={"welcome to home page"} />
+ 
       </Routes>
     </BrowserRouter>
 
