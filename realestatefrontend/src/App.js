42d711b8b7f7ec8941efda1d00a00a2fbd514bac
Amul <shindeamul76@gmail.com>
2022-12-29 10:06:36 +0530

    Created the sidebar and property display

--- realestatefrontend/src/App.js
+++ realestatefrontend/src/App.js
@@ -1,3 +1,4 @@
+
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
 import Home from "./DisplayProp/Home";
@@ -9,8 +10,6 @@ function App() {
     {/* <Sidebar/> */}
     <Home/>
     </BrowserRouter>
-  );
+  )
 }
-
 export default App;
-
