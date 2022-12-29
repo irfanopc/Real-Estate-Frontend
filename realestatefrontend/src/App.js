09555080b1eeb5d0bac5ebf05f36891de4b1564a
Amul <shindeamul76@gmail.com>
2022-12-29 21:45:58 +0530

    created home page and tried to fetch data from backend for displaying property

--- realestatefrontend/src/App.js
+++ realestatefrontend/src/App.js
@@ -1,17 +1,24 @@
 
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; 
 import Signup from "./signUp/Signup";
 import Signin from './signUp/Signin';
+import Home from './DisplayProp/Home';
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+
+   
+      
+ <BrowserRouter>
       <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/Signup" element={<Signup/>} />
+        <Route path="/Signup" element={<Signup/>} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> 
     
     </div>
   );
