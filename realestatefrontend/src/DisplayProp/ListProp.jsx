1ee97a7e5995f3b436c8667c7ae0a7cebe9c365b
irfan opc <irfanopc555@gmail.com>
2022-12-31 11:48:28 +0530

    got user deatils by id

--- realestatefrontend/src/DisplayProp/ListProp.jsx
+++ realestatefrontend/src/DisplayProp/ListProp.jsx
@@ -10,17 +10,33 @@ import axios from "axios";
 const ListProp = () => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("/userdetails")
-      .then( (response) =>{
-      })
-      .catch( (error) =>{
-        console.log(error);
-      });
+  // useEffect(() => {
+  //   axios
+  //     .get("/userdetails")
+  //     .then( (response) =>{
+  //       console.log(response);
+  //     })
+  //     .catch( (error) =>{
+  //       console.log(error);
+  //     });
 
-  }, []);
- 
+  // }, []);
+
+  const [posts, setPosts] = useState();
+  useEffect(() => {
+    
+    fetch("http://localhost:5000/userdetails/63adb6d33704aa9d7dcfb74c")
+      .then((res) => res.json())
+      .then((data) => {
+    let userDetails =  (data.userProperties);
+        setPosts(userDetails.map((data)=>{return (data)}));
+        // console.log(posts);
+      })
+      .catch((error) => {
+        if (error) console.log(error);
+      });
+  }, [])
+  console.log(posts);
 
   return (
     <div className="mainList">
@@ -36,42 +52,32 @@ const ListProp = () => {
         <div>Action</div>
       </div>
 
-      <div className="dislist">
-        <div>PPD125</div>
-        <div>
-          <img src={Imglogo} alt={"img"} />
-        </div>
-        <div>Plot</div>
-        <div>93701723482</div>
-        <div>1200</div>
-        <div>22</div>
-        <div>Sold</div>
-        <div>35</div>
-        <div className="action">
-          <img src={Eyelogo} alt="eye" />
+     
+        {posts &&
+          posts.map((data) => {
+            return (
+              <>
+               <div className="dislist"></div>
+                <div>{data.ppdId}</div>
+                <div>
+                  <img src={Imglogo} alt={"img"} />
+                </div>
+                <div>Plot</div>
+                <div>{data.mobile}</div>
+                <div>1200</div>
+                <div>{data.views}</div>
+                <div>{data.status}</div>
+                <div>{data.daysLeft}</div>
+                <div className="action">
+                  <img src={Eyelogo} alt="Eye" />
 
-          <img src={Editlogo} alt="edit" />
-        </div>
+                  <img src={Editlogo} alt="Edit" />
+                </div>
+              </>
+            );
+          })}
       </div>
-
-      <div className="dislist">
-        <div>PPD125</div>
-        <div>
-          <img src={Imglogo} alt={"img"} />
-        </div>
-        <div>Plot</div>
-        <div>93701723482</div>
-        <div>1200</div>
-        <div>22</div>
-        <div>Sold</div>
-        <div>35</div>
-        <div className="action">
-          <img src={Eyelogo} alt="Eye" />
-
-          <img src={Editlogo} alt="Edit" />
-        </div>
-      </div>
-    </div>
+   
   );
 };
 
