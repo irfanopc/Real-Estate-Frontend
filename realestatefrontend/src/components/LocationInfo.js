743de3d3374616db2fe2eb90749832cf1143aa2c
Manish kumar <manishkumar7348292@gmail.com>
2022-12-31 10:22:33 +0530

    Complited Genraal info Project

--- realestatefrontend/src/components/LocationInfo.js
+++ realestatefrontend/src/components/LocationInfo.js
@@ -7,110 +7,122 @@ import "./LocationInfo.css";
 
 const LocationInfo = () => {
     return (
-        <div className="LocationInfo">
 
 
-            <span>
+<>
+<div className="maincontainer">
+      <div className="row">
+          <div>
+            <span>Email</span>
 
-
-                <div className="leftSide">
-                    Email
-                    <input id="selectBox" placeholder="Email" />
-                </div>
-
-
-                <div className="leftSide">
-                    <label>
-                        Area
-
-                        <select id="selectBox">
-                            <option>Rectangle</option>
-                            <option>Square</option>
-                            <option>Trangle</option>
-                        </select>
-                    </label>
-                </div>
-
-
-
-                <div className="leftSide">
-                    Address
-                    <input id="selectBox" placeholder="Address" />
-                </div>
-
-
-
-                <div className="leftSide">
-                    Latitude
-                    <input id="selectBox" placeholder=" Latitude" />
-                </div>
-                
-                <div className="previous">
-             <button>Previous</button>
-          </div>
-            </span>
-
-
-
-
-
-
-
-
-
-
-
-            <span>
-
-
-
-                <div className="rightSide">
-
-                    <label>
-                        City
-                        <select id="selectBox">
-                            <option>Delhi</option>
-                            <option>Ranchi</option>
-                            <option>Sambalpur</option>
-
-                        </select>
-                    </label>
-
-                </div>
-
-                <div className="rightSide">
-
-                    <label>
-                        Pincode
-                        <select id="selectBox">
-                            <option>834002</option>
-                            <option>768001</option>
-                            <option>834001</option>
-
-                        </select>
-                    </label>
-
-                </div>
-                <div className="rightSide">
-                    Landmark
-
-                    <input id="selectBox" placeholder=" Landmark" />
-                </div>
-
-
-                <div className="rightSide">
-                    Longitude
-
-                    <input id="selectBox" placeholder=" Logitude" />
-                </div>
-
-                <div className="saveContinue">
-             <button>Add Property</button>
+            <div className="selectBox">
+                     <input className="selectBox" placeholder="Email" />
+            </div>
           </div>
 
-
-</span>
+          <div>
+            <span>City</span>
+            <div className="selectBox">
+              <select defaultValue={"toilet"} className="selectBox">
+                <option value={"toilet"}>Select City</option>
+                <option>Delhi</option>
+                <option>Ranchi</option>
+              </select>
+            </div>
+          </div>
         </div>
+
+
+
+
+        <div className="row">
+          <div>
+            <span> Area</span>
+
+            <div className="selectBox">
+              <select defaultValue={"toilet"} className="selectBox">
+                <option value={"toilet"}>Select Area</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <span>Pincode</span>
+            <div className="selectBox">
+              <select defaultValue={"toilet"} className="selectBox">
+                <option value={"toilet"}>Select Pincode</option>
+                <option>834004</option>
+                <option>834001</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row">
+          <div>
+            <span>Address</span>
+
+            <div className="selectBox">
+    
+              <input className="selectBox" placeholder="Address" />
+            </div>
+          </div>
+
+          <div>
+            <span>Landmark</span>
+            <div className="selectBox">
+             
+              <input className="selectBox" placeholder="Landmark" />
+            </div>
+          </div>
+        </div>
+
+
+
+        <div className="row">
+          <div>
+            <span>Latitude</span>
+
+            <div className="selectBox">
+             
+
+<input className="selectBox" placeholder="Latitude" />
+            </div>
+          </div>
+
+          <div>
+            <span>Longitude</span>
+            <div className="selectBox">
+             
+
+<input className="selectBox" placeholder="Latitude" />
+            </div>
+          </div>
+        </div>
+
+        
+
+
+        
+
+       
+      </div>
+
+      <div className="newbuttoncontainer">
+      <div className="newbutton">
+            <button>Previous</button>
+      </div>
+
+      <div className="newbutton">
+            <button>Save & Continue</button>
+        </div>
+  
+      
+      </div>
+</>
 
     );
 };
--- realestatefrontend/src/components/PropertyDetail.jsx
+++ realestatefrontend/src/components/PropertyDetail.jsx
@@ -7,188 +7,181 @@ import "./PropertyDetail.css";
 
 const PropertyDetail = () => {
   return (
-    <div className="PropertyDetail">
 
+<>
+<div className="maincontainer">
+      <div className="row">
+          <div>
+            <span>Length</span>
 
-      <span>
-        <div className="leftSide">
-          Length
-          <input id="selectBox" placeholder="Example:1000" />
-        </div>
-
-
-        <div className="leftSide">
-          Total Area
-          <input id="selectBox" placeholder="Example:75000" />
-        </div>
-
-        <div className="leftSide">
-          <label>
-            No of BHK
-
-            <select id="selectBox">
-              <option>1 BHK</option>
-              <option>2 BHK</option>
-              <option>3 BHK</option>
-            </select>
-          </label>
-        </div>
-
-
-        <div className="leftSide">
-          <label>
-            Attached
-
-            <select id="selectBox">
-              <option>Kitchen</option>
-              <option>Balkani</option>
-              <option>CarParking</option>
-            </select>
-          </label>
-        </div>
-
-
-        <div className="leftSide">
-          <label>
-            Furnished
-
-            <select id="selectBox">
-              <option>Not Furnished</option>
-              <option>Semi Furnished</option>
-              <option>Full Furnished</option>
-
-            </select>
-          </label>
-        </div>
-
-
-        <div className="leftSide">
-          <label>
-            Lift
-
-            <select id="selectBox">
-              <option>Yes</option>
-              <option>No</option>
-            </select>
-          </label>
-        </div>
-
-
-
-        <div className="leftSide">
-          <label>
-            Facing
-
-            <select id="selectBox">
-              <option>Yes</option>
-              <option>No</option>
-            </select>
-          </label>
-        </div>
-
-        <div className="previous">
-     <button>Previous</button>
-     </div>
-
-      </span>
-
-      {/* /Right Side part  =======================>>>>      */}
-
-      
-
-      <span>
-
-
-
-        <div className="rightSide">
-          Breadth
-
-          <input id="selectBox" placeholder="Example:1000" />
-        </div>
-
-        <div className="rightSide">
-
-          <label>
-            Area Unit
-            <select id="selectBox">
-              <option>Square centimeter</option>
-              <option>square decimeter</option>
-              <option>Square meter</option>
-
-            </select>
-          </label>
-
-        </div>
-
-
-
-
-
-
-<div className="rightSide">
-
-          <label>
-            No of Floor
-            <select id="selectBox">
-              <option>First Floor</option>
-              <option>Second Floor</option>
-              <option>Third Floor</option>
-
-            </select>
-          </label>
-
-        </div>
-
-
-
-        <div className="rightSide">
-
-          <label>
-            Westorn Toilet
-            <select id="selectBox">
-              <option>Yes</option>
-              <option>NOr</option>
-            </select>
-          </label>
-
-        </div>
-
-
-
-        <div className="rightSide">
-
-          <label>
-            Car parking
-            <select id="selectBox">
-              <option>Yes</option>
-              <option>No</option>
-            </select>
-          </label>
-
-        </div>
-
-
-        <div className="rightSide">
-          Electricity
-          <input id="selectBox" placeholder="Example: 3 Phase" />
-        </div>
-      
-      
-
-       
-        <div className="saveContinue">
-             <button>Save & Continue</button>
+            <div className="selectBox">
+              
+               <input className="selectBox" placeholder="Example: 1000" />
+            </div>
           </div>
-        
+
+          <div>
+            <span>Breadth</span>
+            <div className="selectBox">
+            
+               <input className="selectBox" placeholder="Example: 1000" />
+            </div>
+          </div>
+        </div>
+
+
+
+
+        <div className="row">
+          <div>
+            <span>Total Area</span>
+
+            <div className="selectBox">
+            <input className="selectBox" placeholder="Example: 7500" />
+            </div>
+          </div>
+
+          <div>
+            <span>Area Unit</span>
+            <div className="selectBox">
+              <select defaultValue={"Area"} className="selectBox">
+                <option value={"Area"}>Area Unit</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row">
+          <div>
+            <span>No Of BHK</span>
+
+            <div className="selectBox">
+              <select defaultValue={"BHK"} className="selectBox">
+                <option value={"BHk"}>Select No fo BHK</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <span>No Of Floor</span>
+            <div className="selectBox">
+              <select defaultValue={"Floor"} className="selectBox">
+                <option value={"floor"}>Select No fo Floor</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div className="row">
+          <div>
+            <span>Attached</span>
+
+            <div className="selectBox">
+              <select defaultValue={"Attached"} className="selectBox">
+                <option value={"Attached"}>Select Attached</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <span>Weastern Toilet</span>
+            <div className="selectBox">
+              <select defaultValue={"toilet"} className="selectBox">
+                <option value={"toilet"}> Select Western Toilet</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div>
+            <span>Furnished</span>
+
+            <div className="selectBox">
+              <select defaultValue={"Furnished"} className="selectBox">
+                <option value={"Furnished"}>Select Furnished</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <span>Car Parking</span>
+            <div className="selectBox">
+              <select defaultValue={"Parking"} className="selectBox">
+                <option value={"Parking"}>Select Car Parking</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row">
+          <div>
+            <span>Lift</span>
+
+            <div className="selectBox">
+              <select defaultValue={"Lift"} className="selectBox">
+                <option value={"Lift"}> Select Lift</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <span>Electricity</span>
+            <div className="selectBox">
+            <input className="selectBox" placeholder="Example: 3 phase" />
+            </div>
+          </div>
+        </div>
+
+        <div className="rowdiff">
+          <div>
+            <span>Facing</span>
+
+            <div className="selectBox">
+              <select defaultValue={"Facing"} className="selectBox">
+                <option value={"Facing"}>Select Facing</option>
+                <option>Yes</option>
+                <option>NO</option>
+              </select>
+            </div>
+          </div>
+
+          
+        </div>
+      </div>
+
+      <div className="newbuttoncontainer">
+      <div className="newbutton">
+            <button>Previous</button>
+      </div>
+
+      <div className="newbutton">
+            <button>Save & Continue</button>
+        </div>
+  
       
-        
-
-        </span>
-        
-        
-         
-
-</div>
+      </div>
+</>
 
 
   );
