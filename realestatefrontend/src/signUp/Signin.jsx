cdc14aca01880e02c1c33ea590d7e299435c2c53
irfanopc <106423601+irfanopc@users.noreply.github.com>
2023-01-04 20:38:52 +0530

    Update Signin.jsx

--- realestatefrontend/src/signUp/Signin.jsx
+++ realestatefrontend/src/signUp/Signin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Signup.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signin() {
   const [passwordShown, setPasswordShown] = useState(false);
