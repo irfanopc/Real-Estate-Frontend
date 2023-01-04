19fd97c2ec22d61549a4387b801a3eeb858a7277
Amul <shindeamul76@gmail.com>
2023-01-04 14:18:09 +0530

    created some basic things which needs to be done

--- realestatefrontend/src/App.js
+++ realestatefrontend/src/App.js
@@ -10,7 +10,6 @@ import Signin from './signUp/Signin';
 import PropertyDetail from './components/PropertyDetail';
 import GeneralInfo from './components/GeneralInfo';
 import LocationInfo from './components/LocationInfo';
-
 import Home from './DisplayProp/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BasicInfo from './components/BasicInfo';
@@ -20,25 +19,17 @@ function App() {
   return (
 
  <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/Signup" element={<Signup/>} />
+ <Routes>
+   <Route path="/" element={<Signin />} />
+   <Route path="/Signup" element={<Signup/>}/>
+   <Route path="/home" element={<Home/>} />
+   <Route path="/BasicInfo" element={<BasicInfo/>} /> 
+   <Route path="/PropertyDetail" element={<PropertyDetail/>} />
+   <Route path="/GeneralInfo" element={<GeneralInfo/>} />
+    <Route path="/LocationInfo" element={<LocationInfo/>} />
+ </Routes>  
+</BrowserRouter>
 
-        <Route path="/PropertyDetail" element={<PropertyDetail/>} />
-        <Route path="/GeneralInfo" element={<GeneralInfo/>} />
-
-        <Route path="/LocationInfo" element={<LocationInfo/>} />
-      </Routes>  
-
-     
-
-        <Route path="/Signup" element={<Signup/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/BasicInfo" element={<BasicInfo/>} />
-      </Routes>
-
-    </BrowserRouter>
-     
   );
 }
 
--- realestatefrontend/src/DisplayProp/Property.jsx
+++ realestatefrontend/src/DisplayProp/Property.jsx
@@ -1,29 +1,64 @@
-import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Uslogo from "./HomeLogo/uslogo.svg";
 import "./Property.css";
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const Property = () => {
+ 
+
+  const navigator = useNavigate()
+
+  let email = window.localStorage.getItem('email');
+  let setemail = email.split('@')[0]
+
+
+
+    const handleLogout =  () => {
+      axios.get('/api/v1/signout')
+        .then((data)=> {
+          localStorage.removeItem('email')
+          localStorage.removeItem('id')
+           toast.success(data.data.massage);
+          navigator("/");
+        })
+        .catch((error)=>{
+          console.log(error);
+        })
+    
+      
+    }
+
+
+  
   return (
     <div className="display-property">
       <div className="user">
-        <div className="right">USER ID : 06PPD125</div>
+        <div className="right">
+          USER ID : 06PPD125
+          </div>
         <div className="left">
           <section>
             <div>
               <img src={Uslogo} alt={"User"} />
             </div>
             <div className="select">
-              <select defaultValue={'Username'}>
+              <select defaultValue={'Username'} onChange={handleLogout}>
                 <option value="Username">
-                  User Name
+                 {`${setemail}`} 
                 </option>
-                <option value="Sign-Out">Sign Out</option>
+                <option value={'Sign Out'} >
+                Sign Out
+                </option>
               </select>
             </div>
           </section>
         </div>
       </div>
+      <ToastContainer/>
     </div>
+
   );
 };
 
